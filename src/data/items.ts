4634ce2f3e2bb98f1c53e9fd1c14c8f0d1b987f4
unknown <brennerpaiva@gmail.com>
2023-06-07 17:39:29 -0300

    first commit

---
+++ src/data/items.ts
@@ -0,0 +1,28 @@
+import { Item } from '../types/Item';
+
+export const items: Item[] = [
+  {
+    date: new Date(2023, 7, 6),
+    category: 'food',
+    title: 'BurguerKing',
+    value: 32.12,
+  },
+  {
+    date: new Date(2023, 7, 6),
+    category: 'food',
+    title: 'MCDonalds',
+    value: 28.99,
+  },
+  {
+    date: new Date(2023, 7, 6),
+    category: 'rent',
+    title: 'Aluguel Apt',
+    value: 2300,
+  },
+  {
+    date: new Date(2023, 8, 21),
+    category: 'salary',
+    title: 'Salário Teste',
+    value: 2300,
+  },
+];
